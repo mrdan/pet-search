d91cf19790c0c41393c2e2b698fbf70aa094203c
Daniel Doyle <daniel.doyle@gmail.com>
2012-01-21 14:47:19 +0000

    Basic style

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <HTML>
-<HEAD><TITLE>Pet Search Test</TITLE></HEAD>
+<HEAD><TITLE>Pet Search Test</TITLE>
+<LINK rel="stylesheet" href="style.css" />
+</HEAD>
 <BODY>
 <?php
 
+// remove duplicate tags from an array
 function array_unique_compact($a) {
   $tmparr = array_unique($a);
   $i=0;
@@ -12,6 +15,30 @@ function array_unique_compact($a) {
   }
   return $newarr;
 }
+
+// print all tags from postings table to the page
+function output_all_tags($DBname,$linkid) {
+    mysql_select_db($DBname,$linkid);
+    $result = mysql_query("SELECT * FROM postings");
+    
+    $tags = array();
+
+    while($row = mysql_fetch_array($result)) {
+        $tags = array_merge($tags, explode(" ", $row['tags']));
+    }
+
+    $tags = array_unique_compact($tags);
+
+    for($i=0;$i<count($tags);$i++) {
+        echo $tags[$i];
+        if($i<count($tags)-1) echo ",\n";
+    }
+}
+
+
+//
+// Main
+//
 
 $DBhost = "127.0.0.1";
 $DBuser = "pet_user";
@@ -23,21 +50,15 @@ if (!$linkid) {
     die("Unable to connect to database".mysql_error());
 }
 
-mysql_select_db($DBname,$linkid);
-$result = mysql_query("SELECT * FROM postings");
+// Begin site
 
-$tags = array();
+echo "<div class'container'>";
+output_all_tags($DBname,$linkid);
+echo "</div>";
 
-while($row = mysql_fetch_array($result)) {
-    $tags = array_merge($tags, explode(" ", $row['tags']));
-}
 
-$tags = array_unique_compact($tags);
 
-for($i=0;$i<count($tags);$i++) {
-    echo $tags[$i];
-    if($i<count($tags)-1) echo ",\n";
-}
+
 
 mysql_close($linkid);
 
