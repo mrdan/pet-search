4b5465e88304ea8708ad7a3d15a104ee02ff2e95
Daniel Doyle <daniel.doyle@gmail.com>
2012-01-22 16:51:11 +0000

    Added a basic script to create some random data for the db

--- index.php
+++ index.php
@@ -1,60 +1,12 @@
 <HTML>
 <HEAD><TITLE>Pet Search Test</TITLE>
-<LINK rel="stylesheet" href="style.css" />
+<LINK rel="stylesheet" href="includes/style.css" />
+<SCRIPT type="text/javascript" src="includes/jquery.js"></SCRIPT>
+<SCRIPT type="text/javascript" src="includes/custom.js"></SCRIPT>
 </HEAD>
 <BODY>
+<?php @ require_once ('includes/custom.php'); ?>
 <?php
-
-// remove duplicate tags from an array
-function array_unique_compact($a) {
-  $tmparr = array_unique($a);
-  $i=0;
-  foreach ($tmparr as $v) {
-    $newarr[$i] = $v;
-    $i++;
-  }
-  return $newarr;
-}
-
-// print all tags from postings table to the page
-function output_all_tags($DBname,$linkid) {
-    mysql_select_db($DBname,$linkid);
-    $result = mysql_query("SELECT * FROM postings");
-    
-    $tags = array();
-
-    while($row = mysql_fetch_array($result)) {
-        $tags = array_merge($tags, explode(" ", $row['tags']));
-    }
-
-    $tags = array_unique_compact($tags);
-
-    for($i=0;$i<count($tags);$i++) {
-        echo $tags[$i];
-        if($i<count($tags)-1) echo ",\n";
-    }
-}
-
-// print all the postings to the page
-function print_postings($DBname, $linkid, $repeat) {
-    mysql_select_db($DBname, $linkid);
-    
-    for($i = 0; $i < $repeat; $i++) {
-        $result = mysql_query("SELECT * FROM postings");    
-        while($row = mysql_fetch_array($result)) {
-            echo "<DIV class='posting'>";
-                echo "<DIV class='photo'><IMG src='' /></DIV>";
-                echo $row['email'].", ".$row['dateadded'].", ".$row['refreshed'];
-//              echo "<SPAN class='tags'>".$row['tags']."</SPAN>";
-            echo "</DIV>";
-        }
-    }
-}
-
-
-//
-// Main
-//
 
 $DBhost = "127.0.0.1";
 $DBuser = "pet_user";
@@ -67,18 +19,16 @@ if (!$linkid) {
 }
 
 // Begin site
-
 echo "<DIV class='container'>";
     echo "<DIV class='header'>"; echo "Pet Search <IMG src='' />"; echo "</DIV>";
-    echo "<DIV class='intro'>"; echo "</DIV>";
-    echo "<DIV class='main'>";
+    echo "<DIV class='intro'>";
+        echo "I've lost a <A href='#male'>Male</A> / <A href='#female'>Female</A> dog. We want to be able to click male or female and have the list below change to reflect the choice. The list should initially load with everything.";
+    echo "</DIV>";
+    echo "<DIV class='main' id='main'>";
+    echo "<DIV id='loading'>Loading...</DIV>";
         print_postings($DBname, $linkid, 5);
     echo "</DIV>";
 echo "</DIV>";
-
-
-
-
 
 mysql_close($linkid);
 
---
+++ populate.php
@@ -0,0 +1,176 @@
+<?php @ require_once ('includes/custom.php'); ?>
+<?php
+
+// returns an array containing $count email addresses. $unique == 1 means just return unique entries
+function generate_email($count, $unique) {
+    $domain = Array("gmail.com","fakemail.com","hotmail.com","mail.com",
+                    "netryst.org","eircom.net","aol.com","pet-search.ie",
+                    "intel.com","google.com","microsoft.com","mediatemple.com",
+                    "sdgsdgsd.edu");
+                    
+    $firstnames = Array("dan","daniel","sandra","aoife","aisling","danny","helen",
+                        "des","desmond","aideen","alan","al","maeve","michael","mike",
+                        "philip","phil","pip","michelle","donal","shane","david","anne");
+                        
+    $lastnames = Array("doyle","lyons","dowling","ryan","hilliard","murphy","white","whitely",
+                       "curran","baker","gleeson");
+                       
+    $adjectives = Array("freaky","drunk","funny","shy","stoned","sexy","dynamite");
+
+    $userlist = Array();
+    
+    for($i = 0; $i < $count; $i++) {
+        $user_domain = $domain[array_rand($domain,1)];
+        $user_first = $firstnames[array_rand($firstnames,1)];
+        $user_last = $lastnames[array_rand($lastnames,1)];
+        $user_adj = $adjectives[array_rand($adjectives,1)];
+
+        $rand = rand(0,100);
+        $rand2 = rand(0,10);
+        
+        if ($rand < 5) 
+        {
+            $username = $user_adj;                  //freaky@
+        } 
+        else if ($rand < 10) 
+        {
+            if ($rand2 < 3)
+                $username = $user_adj."-".$user_first;      //freaky-dan@
+            else if($rand2 < 6)
+                $username = $user_adj.".".$user_first;      //freaky.dan@
+            else
+                $username = $user_adj.$user_first;      //freakydan@
+        } 
+        else if ($rand < 20) 
+        {
+            $username = $user_last.$user_first;     //doyledan@
+        } 
+        else if ($rand < 25) 
+        {
+            if ($rand2 < 4)
+                $username = $user_last."-".$user_first; //doyle-dan@
+            else
+                $username = $user_last.".".$user_first; //doyle.dan@
+        } 
+        else if ($rand < 50) 
+        {
+            $username = $user_first.$user_last;      //dandoyle@
+        } 
+        else if ($rand < 70) 
+        {
+            if ($rand2 < 3)
+                $username = $user_first."-".$user_last; //dan-doyle@
+            else
+                $username = $user_first.".".$user_last; //dan.doyle@
+        } 
+        else 
+        {
+            $username = $user_first;                //dan@
+        }
+        $username = $username."@".$user_domain;
+        
+        $userlist[$i] = $username;
+    }
+    
+    if ($unique == 1)
+        return array_unique_compact($userlist);
+    else
+        return $userlist;
+}
+
+// returns an array containing $count strings containing a set of pet tags. $unique == 1 means return only unique sets (doesn't compare order of tags)
+function generate_pet($count, $unique) {
+    $species = Array("dog","cat","goat","giraffe");
+    $descrip = Array("long-haired","brown","lazy","playful","short-haired","black","white");
+    
+    $petlist = Array();
+    $descrip_size = count($descrip);
+  
+    for($i = 0; $i < $count; $i++) {
+        $pet_descrip = Array();
+        $pet_descrip_keys = Array();
+        $descrip_string = "";
+
+        $pet_species = $species[array_rand($species,1)];                    // take one species
+        $specificity = rand(1,$descrip_size);                               // decide how many tags we want this post to have (at least one, not counting species)
+
+        if ($specificity == 1)
+            $pet_descrip_keys = Array(array_rand($descrip,$specificity));   // there was a horrible bug here. it's gone now. RIP 22-01-2012.
+        else
+            $pet_descrip_keys = array_rand($descrip,$specificity);          // get a $specificity sized set of keys to use to grab tags
+
+        for($x=0;$x < count($pet_descrip_keys);$x++) {
+            $pet_descrip[$x] = $descrip[$pet_descrip_keys[$x]];
+        }
+        $descrip_string = implode(" ", $pet_descrip);
+        $petlist[$i] = $pet_species." ".$descrip_string;
+    }
+    
+    if($unique == 1)
+        return array_unique_compact($petlist);
+    else
+        return $petlist;
+}
+
+//
+// Main
+//
+$DBhost = "127.0.0.1";
+$DBuser = "pet_user";
+$DBpass = "234rewf2";
+$DBname = "petsearch";
+
+$linkid = mysql_connect($DBhost,$DBuser,$DBpass);
+if (!$linkid) {
+    die("Unable to connect to database".mysql_error());
+}
+
+$counter = $_GET['c'];
+$unique = $_GET['u'];
+
+if($counter == NULL) {
+    $userlist = generate_email(100, $unique);
+    $petlist = generate_pet(100, $unique);
+} else {
+    $userlist = generate_email($counter, $unique);
+    $petlist = generate_pet($counter, $unique);
+}
+
+$usercount = count($userlist);
+$petcount = count($petlist);
+
+// Connect our lists
+if ($usercount < $petcount) {
+    $petlist = array_slice($petlist, 0, $usercount);
+} elseif ($usercount > $petcount) {
+    $userlist = array_slice($userlist, 0, $petcount);
+}
+$combolist = array_combine($userlist, $petlist);
+
+// Print stats
+if($unique == 1) {
+    echo "Unique users: ".$usercount."<BR />";
+    echo "Unique pets: ".$petcount."<BR />";
+    echo "Combined total is always the smallest number. <BR /><BR />";
+}
+
+// Spit out list
+reset($combolist);
+while (list($u,$p) = each($combolist)) {
+    echo $u.": ".$p."<BR />";
+}
+
+
+
+// date && refreshed
+// random date before X and after Y
+// random refreshed number less than 10
+// date refreshed should not be the same as dateadded if refreshed = 0
+
+
+mysql_select_db($DBname, $linkid);
+
+//mysql_query("INSERT INTO");
+
+mysql_close($linkid);
+?>
