5b0c41f58d04d8ceb6a2c4532b112a0027aa846f
Daniel Doyle <daniel.doyle@gmail.com>
2012-01-26 14:41:19 +0000

    tags are now highlighted based on if theyre in the url, so it sticks even with bookmarking or reloads

--- includes/custom.js
+++ includes/custom.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
 
 
 function tagClick() {
-    $(this).addClass("hilite");
 
     var reg_pattern = '([\/]' + $(this).text() + ')|((?:^#)' + $(this).text() + '[\/]?)';
     var re_this = new RegExp(reg_pattern,'g');
@@ -36,59 +35,39 @@ function tagClick() {
 
 $(function(){
   
-  // Bind an event to window.onhashchange that, when the hash changes, gets the
-  // hash and adds the class "selected" to any matching nav link.
-  $(window).hashchange( function(){
-    var hash = location.hash;
+	// Bind an event to window.onhashchange that, when the hash changes, activates
+  	$(window).hashchange( function() {
+		var hash = location.hash;
     
-    // Set the page title based on the hash.
-    //document.title = 'The hash is ' + ( hash.replace( /^#/, '' ) || 'blank' ) + '.';
-    
-    /*
-    // Iterate over all nav links, setting the "selected" class as-appropriate.
-    $('#nav a').each(function(){
-      var that = $(this);
-      that[ that.attr( 'href' ) === hash ? 'addClass' : 'removeClass' ]( 'selected' );
-    });
-    */
+    	//need to parse the taglist from the anchor
+    	var matchtags = window.location.href.match(/^[^#]+#([a-z,-]+(?:\/[a-z,-]+)*)/);	//TODO: change this to use window.location.hash
+    	if (matchtags != null) {
+    		//pass it to filter.php, we split by / so we need to change them to + for php's sake
+    		query = "tags=" + matchtags[1].replace(/\//g, '+');
+    		$.get("filter.php",query,function(data) {
+    			$("#main").html(data);
+    		});
+    	} else {
+    		//no tags so our query is nothing! take the rest of the day off!
+    		query = "?";
+    		$.get("filter.php",query,function(data) {
+    			$("#main").html(data);
+    		});
+    	}
 
-    //
-    // on hash change we want to:
-    	//call filter.php with the new taglist and repopulate the postings
-    		//need to parse the taglist from the anchor
-    		var matchtags = window.location.href.match(/^[^#]+#([a-z,-]+(?:\/[a-z,-]+)*)/);	//TODO: change this to use window.location.hash
-    		if (matchtags != null) {
-    			console.log(matchtags);
-    			//pass it to filter.php, we split by / so we need to change them to + for php's sake
-    			query = "tags=" + matchtags[1].replace(/\//g, '+');
-    			console.log(query);
-    			//display results
-    			$.get("filter.php",query,function(data) {
-    				$("#main").html(data);
-    			});
-    		} else {
-    			//no tags so our query is nothing! take the rest of the day off!
-    			query = "?";
-    			console.log(query);
-    			//display results
-    			$.get("filter.php",query,function(data) {
-    				$("#main").html(data);
-    			});
-    		};
-    			
-    		
-
-    	//rewrite the urls on the existing tag links so they include the new addition to the anchor and so the selected ones are "hilite"d
-    	$('.tag').each(function(){
+    	//TODO: we need to colour all the right tags
+    	//change a.tag's so the selected ones are "hilite"d
+       	$('a.tag').each(function(){
     		var that = $(this);
-      		that[ that.attr( 'href' ) === hash ? 'addClass' : 'removeClass' ]( 'hilite' );
-    	});
+			var reg_pattern = '([\/]' + that.text() + ')|((?:^#)' + that.text() + '[\/]?)';
+    		var re_this = new RegExp(reg_pattern,'g');
+    		var taginurl = re_this.test(window.location.hash);
 
-    //
-  });
+      		that[ taginurl ? 'addClass' : 'removeClass' ]( 'hilite' );
+    	});
+	});
   
-  // Since the event is only triggered when the hash changes, we need to trigger
-  // the event now, to handle the hash the page may have loaded with.
-  $(window).hashchange();
-  
+  	// Since the event is only triggered when the hash changes, we need to trigger
+  	// the event now, to handle the hash the page may have loaded with.
+  	$(window).hashchange();
 });
